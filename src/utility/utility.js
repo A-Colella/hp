b816e2ef44d59e0afff9ca7fce434cce98611171
Artumeify <a.colella@live.it>
2023-02-22 11:55:41 +0100

    table

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -1,6 +1,6 @@
 export default class CharactersDataService {
     async getCharacterByHouse() {
-        const url = "https://hp-api.onrender.com/api/characters/house/"+(house);
+        const url = "https://hp-api.onrender.com/api/characters/house/gryffindor";
         try {
             const promise = await fetch(url, {method: "GET"});
             const response = await promise.json ();
